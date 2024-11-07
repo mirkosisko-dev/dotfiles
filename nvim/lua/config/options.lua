4093f661844771c4a1addf44008b6d8c25925b6d
mirkosisko-dev <mirkosisko.dev@gmail.com>
2024-11-07 13:38:36 +0100

    feat: line number colors

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -4,3 +4,6 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.wo.number = true
 vim.wo.relativenumber = true
+vim.opt.cursorline = true
+vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#cba6f7" })
+vim.api.nvim_set_hl(0, "LineNr", { fg = "#b4befe" })
