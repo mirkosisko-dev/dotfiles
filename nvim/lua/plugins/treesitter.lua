2ec7225a56ceab3af1099d1837448a0af6e30292
mirkosisko-dev <mirkosisko.dev@gmail.com>
2024-11-26 10:16:55 +0100

    fix: remove eslint_d

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,27 +1,27 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    event = { "BufReadPre", "BufNewFile" },
-    build = ":TSUpdate",
-    dependencies = { "windwp/nvim-ts-autotag" },
-    config = function()
-      local configs = require("nvim-treesitter.configs")
+	{
+		"nvim-treesitter/nvim-treesitter",
+		event = { "BufReadPre", "BufNewFile" },
+		build = ":TSUpdate",
+		dependencies = { "windwp/nvim-ts-autotag" },
+		config = function()
+			require("nvim-ts-autotag").setup()
+			local configs = require("nvim-treesitter.configs")
 
-      configs.setup({
-        auto_install = true,
-        highlight = { enable = true },
-        indent = { enable = true },
-        autotag = { enable = true },
-        incremental_selection = {
-          enable = true,
-          keymaps = {
-            init_selection = "<C-space>",
-            node_incremental = "<C-space>",
-            scope_incremental = false,
-            node_decremental = "<bs>",
-          },
-        },
-      })
-    end,
-  },
+			configs.setup({
+				auto_install = true,
+				highlight = { enable = true },
+				indent = { enable = true },
+				incremental_selection = {
+					enable = true,
+					keymaps = {
+						init_selection = "<C-space>",
+						node_incremental = "<C-space>",
+						scope_incremental = false,
+						node_decremental = "<bs>",
+					},
+				},
+			})
+		end,
+	},
 }
