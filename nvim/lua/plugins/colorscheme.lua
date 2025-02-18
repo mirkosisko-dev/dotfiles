7349cf7fc5aac6e58d5def4eefb9c51079bff8a1
mirkosisko-dev <mirkosisko.dev@gmail.com>
2025-02-18 12:28:23 +0100

    fix: remove unused variable

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,9 +1,21 @@
-local colorscheme = vim.env.COLORSCHEME or "tokyonight"
-
 return {
 	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {
+			style = "night",
+			no_italic = true,
+			dim_inactive = true,
+			plugins = { auto = true },
+		},
+		config = function(_, opts)
+			require("tokyonight").setup(opts)
+			vim.cmd.colorscheme("tokyonight-night")
+		end,
+	},
+	{
 		"catppuccin/nvim",
-		enabled = colorscheme == "catppuccin",
 		name = "catppuccin",
 		lazy = true,
 		opts = {
@@ -14,28 +26,6 @@ return {
 		config = function(_, opts)
 			require("catppuccin").setup(opts)
 			vim.cmd.colorscheme("catppuccin-mocha")
-		end,
-	},
-	{
-		"folke/tokyonight.nvim",
-		enabled = colorscheme == "tokyonight",
-		lazy = false,
-		priority = 1000,
-		opts = {
-			style = "night",
-			no_italic = true,
-			dim_inactive = true,
-			plugins = { auto = true },
-			-- styles = {
-			-- 	comments = "none",
-			-- 	keywords = "none",
-			-- 	functions = "none",
-			-- 	variables = "none",
-			-- },
-		},
-		config = function(_, opts)
-			require("tokyonight").setup(opts)
-			vim.cmd.colorscheme("tokyonight-night")
 		end,
 	},
 }
