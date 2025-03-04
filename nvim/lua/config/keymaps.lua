83c9f74141117a18629a00d6dbb49ad438f9d7dd
mirkosisko-dev <mirkosisko.dev@gmail.com>
2025-03-04 13:26:39 +0100

    feat: delete without adding to register & markdown

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -16,11 +16,11 @@ keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Center cursor after moving up half
 keymap.set("n", "n", "nzzzv", { desc = "Jump to next search term" })
 keymap.set("n", "N", "Nzzzv", { desc = "Jump to previous search term" })
 
--- TIP: Disable arrow keys in normal mode
-keymap.set("n", "<left>", '<cmd>echo "Use h to move!!"<CR>')
-keymap.set("n", "<right>", '<cmd>echo "Use l to move!!"<CR>')
-keymap.set("n", "<up>", '<cmd>echo "Use k to move!!"<CR>')
-keymap.set("n", "<down>", '<cmd>echo "Use j to move!!"<CR>')
+-- Disable arrow keys in normal mode
+keymap.set("n", "<left>", '<cmd>echo "Use h to move"<CR>')
+keymap.set("n", "<right>", '<cmd>echo "Use l to move"<CR>')
+keymap.set("n", "<up>", '<cmd>echo "Use k to move"<CR>')
+keymap.set("n", "<down>", '<cmd>echo "Use j to move"<CR>')
 
 -- Shift + J/K moves selected lines down/up in visual Mode
 keymap.set("v", "J", ":m '>+1<CR>gv=gv", { noremap = true, silent = true, desc = "Move selected line down" })
@@ -29,3 +29,13 @@ keymap.set("v", "K", ":m '<-2<CR>gv=gv", { noremap = true, silent = true, desc =
 -- Open Todos
 keymap.set("n", "<leader>owt", ":vsplit ~/work/todo<cr>", { desc = "Open work todos" })
 keymap.set("n", "<leader>opt", ":vsplit ~/personal/todo<cr>", { desc = "Open personal todos" })
+
+-- Resize Windows
+keymap.set("n", "=", [[<cmd>vertical resize +5<cr>]], { desc = "Make the window bigger vertically" })
+keymap.set("n", "-", [[<cmd>vertical resize -5<cr>]], { desc = "Make the window smaller vertically" })
+keymap.set("n", "+", [[<cmd>horizontal resize +2<cr>]], { desc = "Make the window bigger horizontally" })
+keymap.set("n", "_", [[<cmd>horizontal resize -2<cr>]], { desc = "Make the window smaller horizontally" })
+
+-- Delete without copying
+keymap.set({ "n", "v" }, "<leader>x", '"_x', { desc = "Delete character without copying into register" })
+keymap.set({ "n", "v" }, "<leader>X", '"_dd', { desc = "Delete line without copying into register" })
